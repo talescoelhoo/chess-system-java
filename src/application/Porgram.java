8ed3e221b3dae2875acbd7a2271cbd2d3ee75f1f
tales coelho <talisbastosmma@gmail.com>
2025-02-24 13:08:30 -0300

    Chess layer and printing the board

--- src/application/Porgram.java
+++ src/application/Porgram.java
@@ -1,13 +1,13 @@
 package application;
 
-import bordgame.Board;
-import bordgame.Position;
+import chess.ChessMatch;
 
 public class Porgram {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(4, 4);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,15 @@
+package chess;
+
+import bordgame.Board;
+import bordgame.Piece;
+
+public class ChessPiece extends Piece{
+	
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+	
+}
