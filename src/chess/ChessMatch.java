8823a3c453f0d671b8a9cbda78485e0ab928f819
tales coelho <talisbastosmma@gmail.com>
2025-03-05 22:58:14 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,10 +23,14 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	
+
+	private void placeNewPicie(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+
 	private void incialSetup() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		placeNewPicie('b', 6, new Rook(board, Color.WHITE));
+		placeNewPicie('e', 8, new King(board, Color.BLACK));
+		placeNewPicie('e', 1, new King(board, Color.WHITE));
 	}
 }
