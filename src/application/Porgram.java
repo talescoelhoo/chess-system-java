5e640d16f86593a0cedb4e1aa9db31757d17b6c3
tales coelho <talisbastosmma@gmail.com>
2025-03-30 14:07:41 -0300

    Handling captured pieces

--- src/application/Porgram.java
+++ src/application/Porgram.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -11,14 +13,15 @@ import chess.ChessPosition;
 public class Porgram {
 
 	public static void main(String[] args) {
-
-		ChessMatch chessMatch = new ChessMatch();
+		
 		Scanner sc = new Scanner(System.in);
+		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList();
 		
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -32,6 +35,9 @@ public class Porgram {
 				ChessPosition target = UI.readChessPosition(sc);
 	
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
